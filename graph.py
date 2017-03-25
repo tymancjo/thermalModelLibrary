9e7832c31f73ecb93672d723b9195947a0d9ef86
tymancjo <tomasz.tomanek@gmail.com>
2017-03-25 15:55:18 +0100

    working version of the base GIU for array draw and save / open. graph.py

--- graph.py
+++ graph.py
@@ -9,26 +9,27 @@ canvas_width = 500
 canvas_height = 500
 
 elementsInX = 10
-elementsInY = 20
+elementsInY = 10
 
 
 
 dX = int(canvas_width / elementsInX)
 dY = int(canvas_height / elementsInY)
 
-XSecArray = np.zeros(shape=[elementsInY-2,elementsInX-2])
+XSecArray = np.zeros(shape=[elementsInY,elementsInX])
 
 def checkered(canvas, line_distanceX, line_distanceY):
    # Cleaning up the whole space
    w.create_rectangle(0, 0, canvas_width, canvas_height, fill="white", outline="gray")
    # vertical lines at an interval of "line_distance" pixel
-   for x in range(line_distanceX,canvas_width,line_distanceX):
-      canvas.create_line(x, line_distanceY, x, canvas_height-line_distanceY, fill="gray")
+   for x in range(0,canvas_width,int(line_distanceX)):
+      canvas.create_line(x, 0, x, canvas_height, fill="gray")
    # horizontal lines at an interval of "line_distance" pixel
-   for y in range(line_distanceY,canvas_height,line_distanceY):
-      canvas.create_line(line_distanceX, y, canvas_width-line_distanceX, y, fill="gray")
+   for y in range(0,canvas_height,int(line_distanceY)):
+      canvas.create_line(0, y, canvas_width, y, fill="gray")
 
-
+def arraySlicer(inputArray, subDivisions):
+    return inputArray.repeat(subDivisions,axis=0).repeat(subDivisions,axis=1)
 
 def showXsecArray(event):
     print(XSecArray)
@@ -45,6 +46,7 @@ def displayArrayAsImage():
     # plt.show()
 
 def clearArrayAndDisplay():
+    global XSecArray, dX, dY
     if np.sum(XSecArray) != 0: # Test if there is anything draw on the array
         q = messagebox.askquestion("Delete", "This will delete current shape. Are You Sure?", icon='warning')
         if q == 'yes':
@@ -93,7 +95,7 @@ def setUpPoint( event, Set ):
     Col = int(event.x/dX)
     Row = int(event.y/dY)
 
-    if event.x < canvas_width-dX and event.y < canvas_height-dY and event.x > dX and event.y > dY:
+    if event.x < canvas_width and event.y < canvas_height and event.x > 0 and event.y > 0:
         inCanvas = True
     else:
         inCanvas = False
@@ -101,11 +103,11 @@ def setUpPoint( event, Set ):
 
     if Set and inCanvas:
         w.create_rectangle(Col*dX, Row*dY, Col*dX+dX, Row*dY+dY, fill="orange", outline="gray")
-        XSecArray[Row-1][Col-1] = 1
+        XSecArray[Row][Col] = 1
 
     elif not(Set) and inCanvas:
         w.create_rectangle(Col*dX, Row*dY, Col*dX+dX, Row*dY+dY, fill="white", outline="gray")
-        XSecArray[Row-1][Col-1] = 0
+        XSecArray[Row][Col] = 0
 
 def printTheArray(dataArray):
     global dX, dY
@@ -114,8 +116,8 @@ def printTheArray(dataArray):
     elementsInX = dataArray.shape[1]
 
     # Now we calculate the propper dX and dY for this array
-    dX = int(canvas_width / (elementsInX+2))
-    dY = int(canvas_height / (elementsInY+2))
+    dX = (canvas_width / (elementsInX))
+    dY = (canvas_height / (elementsInY))
 
     # Now we cleanUp the field
     checkered(w, dX, dY)
@@ -127,29 +129,51 @@ def printTheArray(dataArray):
             else:
                 fillColor = "white"
 
-            w.create_rectangle((Col+1)*dX, (Row+1)*dY, (Col+1)*dX+dX, (Row+1)*dY+dY, fill=fillColor, outline="gray")
+            w.create_rectangle((Col)*dX, (Row)*dY, (Col)*dX+dX, (Row)*dY+dY, fill=fillColor, outline="gray")
+
+def subdivideArray():
+    global XSecArray
+    XSecArray = arraySlicer(inputArray = XSecArray, subDivisions = 2)
+    printTheArray(dataArray=XSecArray)
+
+
+########
+
 
 master = Tk()
 master.title( "Cross Section Designer" )
+
+img = PhotoImage(file='CSDico.gif')
+master.tk.call('wm', 'iconphoto', master._w, img)
 
 w = Canvas(master,
            width=canvas_width,
            height=canvas_height)
 w.configure(background='white')
-w.pack(expand = YES, fill = BOTH, side= TOP)
+w.grid(row=0, column=1, columnspan=2, rowspan=10, sticky=W+E+N+S, padx=1, pady=1)
 
-print_button = Button(master, text='Display View', command=displayArrayAsImage)
-print_button.pack(side = LEFT)
+opis = Label(text='Cross Section\n Designer\n v0.1', height=15)
+opis.grid(row=8, column=0,)
 
-print_button_save = Button(master, text='Save to File', command=saveArrayToFile)
-print_button_save.pack()
+emptyOpis = Label(text='', height=3)
+emptyOpis.grid(row=5, column=0,)
 
-print_button_load = Button(master, text='Load from File', command=loadArrayFromFile)
-print_button_load.pack()
+print_button = Button(master, text='Refresh View', command=displayArrayAsImage, height=2, width=16)
+print_button.grid(row=7, column=0, padx=5, pady=5)
 
-print_button_clear = Button(master, text='Clear All', command=clearArrayAndDisplay)
-print_button_clear.pack()
+print_button_slice = Button(master, text='Subdivide', command=subdivideArray, height=2, width=16)
+print_button_slice.grid(row=6, column=0 , padx=5, pady=5)
 
+
+
+print_button_load = Button(master, text='Load from File', command=loadArrayFromFile, height=2, width=16)
+print_button_load.grid(row=2, column=0, padx=5, pady=5)
+
+print_button_save = Button(master, text='Save to File', command=saveArrayToFile, height=2, width=16)
+print_button_save.grid(row=3, column=0, padx=5, pady=5)
+
+print_button_clear = Button(master, text='New Geometry', command=clearArrayAndDisplay, height=2, width=16)
+print_button_clear.grid(row=1, column=0, padx=5, pady=5)
 
 w.bind( "<Button 1>", functools.partial(setUpPoint, Set=True))
 w.bind( "<Button 3>", functools.partial(setUpPoint, Set=False))
@@ -160,7 +184,9 @@ w.bind( "<Button 2>", showXsecArray)
 
 
 message = Label( master, text = "use: Left Mouse Button to Set conductor, Right to reset" )
-message.pack( side = BOTTOM )
+#message.pack( side = BOTTOM )
+message.grid(row=10, column=0, columnspan=2)
+
 master.resizable(width=False, height=False)
 
 checkered(w, dX, dY)
